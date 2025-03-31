6f7673ee8b32e55066ca54fa037e11898beb7b72
Julie Rose <161734285+julierose-NOAA@users.noreply.github.com>
2025-03-31 13:39:33 -0400

    Add report and palette list

---
+++ Resources/NMFS_palette_additions.R
@@ -0,0 +1,35 @@
+#Categorical palettes
+nmfs_cat1 <- c("#003087", "#00797F", "#A5AAAF", "#FFFFFF")
+
+nmfs_cat2 <- c("#056FB7", "#1EBEC7", "#CBCFD1", "#FF8400")
+
+nmfs_cat3 <- c("#3B469A", "#4B8320", "#FFAB38", "#FFFF65")
+
+nmfs_cat4 <- c("#FF6C57", "#737BE6", "#90DFE3", "#F7FAEE")
+
+nmfs_cat5 <- c("#003087", "#B71300", "#00797F", "#A5AAAF", "#FFFFFF")
+
+regions_viz1 <- c("#002364", "#00797F", "#76BC21", "#5761C0", "#DB6015", "#B71300")
+
+regions_viz2 <- c("#002364", "#00797F", "#B1DC6B", "#5761C0", "#DB6015", "#B71300")
+
+#Paired gradient palette
+pairs_gradient <- c("#001743", "#00559B", "#1EBEC7", "#90DFE3", "#DB6015", "#FFAB38")
+
+#Gradient palettes
+oceans_viz <- c("#001743", "#003087", "#00559B", "#0085CA", "#5EB6D9", "#C6E6F0")
+
+waves_viz <- c("#283A38", "#00797F", "#1EBEC7", "#90DFE3")
+
+urchin_viz <- c("#3B469A", "#737BE6", "#A8B8FF", "#F1F2F3")
+
+crustacean_viz1 <- c("#283A38", "#853B00", "#DB6015", "#FFAB38")
+
+crustacean_viz2 <- c("#853B00", "#DB6015", "#FFAB38", "#FFFF65")
+
+crustacean_viz3 <- c("#853B00", "#DB6015", "#FFAB38", "#FFDD6A")
+
+coral_viz1 <- c("#283A38","#901200", "#DB2207", "#FF6C57")
+
+coral_viz2 <- c("#901200", "#DB2207", "#FF6C57", "#FFD1B4")
+
